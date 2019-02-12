120dda163650cb6cdf09ccde45247ce8feb8d877
U-AD\Markus.Meister <Markus.Meister@GERGRMLT172007.ad.insidemedia.net>
2019-02-12 18:42:51 +0100

    _

---
+++ df_funs.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+    
+    DF Funs
+
+    for DB - ETL / API
+
+Created on Tue Feb 12 18:29:58 2019
+@author: Markus.Meister
+"""
+#%% -- imports --
+import numpy as np
+import pandas as pd
+import dask
+import dask.dataframe as dd
+from openpyxl import load_workbook
+#%% -- main funs --
+
+def write_df_to_excel(df, excel_name, sheet_name, startrow=None,
+                       truncate_sheet=False, 
+                       **to_excel_kwargs):
+    
+    # ignore [engine] parameter if it was passed
+    if 'engine' in to_excel_kwargs:
+        to_excel_kwargs.pop('engine')
+
+    writer = pd.ExcelWriter(excel_name, engine='openpyxl')
+
+    # Python 2.x: define [FileNotFoundError] exception if it doesn't exist 
+    try:
+        FileNotFoundError
+    except NameError:
+        FileNotFoundError = IOError
+
+
+    try:
+        # try to open an existing workbook
+        writer.book = load_workbook(excel_name)
+
+        # get the last row in the existing Excel sheet
+        # if it was not specified explicitly
+        if startrow is None and sheet_name in writer.book.sheetnames:
+            startrow = writer.book[sheet_name].max_row
+
+        # truncate sheet
+        if truncate_sheet and sheet_name in writer.book.sheetnames:
+            # index of [sheet_name] sheet
+            idx = writer.book.sheetnames.index(sheet_name)
+            # remove [sheet_name]
+            writer.book.remove(writer.book.worksheets[idx])
+            # create an empty sheet [sheet_name] using old index
+            writer.book.create_sheet(sheet_name, idx)
+
+        # copy existing sheets
+        writer.sheets = {ws.title:ws for ws in writer.book.worksheets}
+    except FileNotFoundError:
+        # file does not exist yet, we will create it
+        pass
+
+    if startrow is None:
+        startrow = 0
+
+    # write out the new sheet
+    df.to_excel(writer, sheet_name, startrow=startrow, **to_excel_kwargs)
+
+    # save the workbook
+    writer.save()
+    writer.close()
+
+
