29eb7399dccf9319e27922c8ca36250231c56e51
U-AD\Markus.Meister1 <Markus.Meister1@GERGRMLT172007.ad.insidemedia.net>
2019-07-09 17:38:38 +0200

    ..

---
+++ df2qtable.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from PyQt5 import QtCore, QtGui ,QtWidgets
+class PandasModel2(QtCore.QAbstractTableModel):
+    """
+    Class to populate a table view with a pandas dataframe
+    """
+    def __init__(self, data, parent=None):
+        QtCore.QAbstractTableModel.__init__(self, parent)
+        self._data = data
+
+    def rowCount(self, parent=None):
+        return self._data.shape[0]
+
+    def columnCount(self, parent=None):
+        return self._data.shape[1]
+
+    def data(self, index, role=QtCore.Qt.DisplayRole):
+        if index.isValid():
+            if role == QtCore.Qt.DisplayRole:
+                return str(self._data.iloc[index.row(), index.column()])
+        return None
+
+    def headerData(self, col, orientation, role):
+        if orientation == QtCore.Qt.Horizontal and role == QtCore.Qt.DisplayRole:
+            return self._data.columns[col]
+        return None
+
+def view_df(df_name):
+    view = QtWidgets.QTableView()
+    view.setModel(PandasModel2(df_name))
+    view.show()
